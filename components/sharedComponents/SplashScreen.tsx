dc98e51e1227f6d0b2036120e7e9e41691f21bb2
erdiha <erdiha@hotmail.com>
2024-04-15 18:11:26 -0700

    reload issue

--- components/sharedComponents/SplashScreen.tsx
+++ components/sharedComponents/SplashScreen.tsx
@@ -1,13 +1,14 @@
-/* eslint-disable react-hooks/exhaustive-deps */
 import { motion } from 'framer-motion';
 import Image from 'next/image';
 import { useRouter } from 'next/router';
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { useMediaQuery } from 'react-responsive';
 import Loading from './Loading';
+
 const SplashScreen: React.FC = () => {
   const router = useRouter();
   const isMobile = useMediaQuery({ maxWidth: 468 });
+  const [showSplashScreen, setShowSplashScreen] = useState(true);
   const isMounted = useRef(true);
 
   useEffect(() => {
@@ -19,31 +20,41 @@ const SplashScreen: React.FC = () => {
   useEffect(() => {
     const timer = setTimeout(() => {
       if (isMounted.current) {
-        router.push('/home');
+        setShowSplashScreen(false);
       }
-    }, 2000); // Delay for 2 seconds before redirecting
+    }, 2000); // Delay for 2 seconds before setting showSplashScreen to false
 
     return () => clearTimeout(timer); // Clean up the timer
   }, []);
 
+  useEffect(() => {
+    if (!showSplashScreen) {
+      router.push('/home');
+    }
+  }, [showSplashScreen]);
+
   return (
-    <div className="flex flex-col items-center justify-evenly h-screen bg-blue-300 text-white w-screen p-10 overflow-hidden">
-      <motion.div
-        initial={{ x: -500 }}
-        animate={{ x: 0 }}
-        transition={{ type: 'spring', damping: 10, stiffness: 70, mass: 2 }}
-      >
-        <Image
-          src="/homerdonut.png"
-          alt="Splash Screen Image"
-          width={isMobile ? 200 : 300}
-          height={isMobile ? 200 : 300}
-          loading='eager'
-          priority={true}
-        />
-      </motion.div>
-      <Loading isMobile={isMobile} isLoading={true} />
-    </div>
+    <>
+      {showSplashScreen ? (
+        <div className="flex flex-col items-center justify-evenly h-screen bg-blue-300 text-white w-screen p-10 overflow-hidden">
+          <motion.div
+            initial={{ x: -500 }}
+            animate={{ x: 0 }}
+            transition={{ type: 'spring', damping: 10, stiffness: 70, mass: 2 }}
+          >
+            <Image
+              src="/homerdonut.png"
+              alt="Splash Screen Image"
+              width={isMobile ? 200 : 300}
+              height={isMobile ? 200 : 300}
+              loading='eager'
+              priority={true}
+            />
+          </motion.div>
+          <Loading isMobile={isMobile} isLoading={true} />
+        </div>
+      ) : null}
+    </>
   );
 };
 
