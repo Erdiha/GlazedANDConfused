34eb693a7deadbac65c1a9a9ea2f200b922ecff9
ERDI HACIOGULLARI <95306736+Erdiha@users.noreply.github.com>
2024-05-02 14:05:32 -0700

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,3 @@
-import { seoInfo } from '@/components/data/texts';
 import { useRouter } from 'next/router';
 import React, { useEffect, useState } from 'react';
 import Layout from '../components/SEO/Layout'; // Import the Layout component
@@ -7,10 +6,21 @@ import Nav from '../components/sharedComponents/Nav';
 import SplashScreen from '../components/sharedComponents/SplashScreen';
 import '../styles/globals.css';
 
+// Decide which metadata import to use based on the project's current structure.
+// Assuming we continue with the pageMetadata as it allows more structured data management.
+import { pageMetadata } from '@/components/SEO/Metadata';
+
 interface AppProps {
   Component: React.ComponentType;
   pageProps: Record<string, unknown>;
 }
+
+// Default SEO information, previously from backup-branch
+const defaultSeoInfo = {
+  title: 'Glazed and Confused - Gourmet Mini Donuts',
+  description: 'Discover Glazed and Confused, your ultimate destination for gourmet mini donuts. Enjoy our wide variety of flavors, perfect for any occasion, from casual gatherings to special events.',
+  keywords: 'gourmet mini donuts, donut catering, fresh donuts, donut truck, specialty donuts, homemade donuts',
+};
 
 const App: React.FC<AppProps> = ({ Component, pageProps }) => {
   const [showSplashScreen, setShowSplashScreen] = useState(true);
@@ -26,8 +36,9 @@ const App: React.FC<AppProps> = ({ Component, pageProps }) => {
   const router = useRouter();
   const { pathname } = router;
 
-  // Retrieve SEO information for the current page
-  const currentSeoInfo: any = seoInfo[pathname] || {};
+  // Using pageMetadata with a fallback to defaultSeoInfo
+  const seoData = pageMetadata[pathname] || defaultSeoInfo;
+
   return (
     <>
       {showSplashScreen ? (
@@ -36,9 +47,9 @@ const App: React.FC<AppProps> = ({ Component, pageProps }) => {
         <div className="relative">
           <Nav />
           <Layout
-            title={currentSeoInfo.title || 'Glazed and Confused'}
-            description={currentSeoInfo.description || 'Default Description'}
-            keywords={currentSeoInfo.keywords}
+            title={seoData.title}
+            description={seoData.description}
+            keywords={seoData.keywords}
           >
             <Component {...pageProps} />
           </Layout>
